cd8bc826637cbcaa230e89fbc4c55bebf23ef18a
adars <adars@LAPTOP-F4RLDE0L>
2020-12-02 00:46:11 +0530

    Spring POC-Setter Injection

---
+++ SProj1-POCSetterInjection/src/test/java/adarsh/SProj1_POCSetterInjection/AppTest.java
@@ -0,0 +1,20 @@
+package adarsh.SProj1_POCSetterInjection;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
